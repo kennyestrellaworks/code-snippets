a04d1ae07a71731b00ff2549ae6e1d845fa0af0e
kennyestrellaworks <kennyestrellaworks@gmail.com>
2026-06-12 14:03:22 +0800

    Compiled snippets into one folder.

---
+++ sort-ascending-numbers/app.js
@@ -0,0 +1,48 @@
+const arrayToSort = [200, 450, -400, 3000, -650, -130, 70, 1300]
+console.log('arrayToSort', arrayToSort)
+let sortAscendedArray = []
+let tempArray = []
+let currentIndex = 0
+let nextIndex = 0
+
+// Checking if current 'smallestNumber' exist in 'arrayToSortCopy'. If not, we push it to 'tempArray' and return it.
+const mutateArrayToSortCopy = function(arrayToSortCopy, smallestNumber) {
+    let tempArray = []
+    for (let i = 0; i < arrayToSortCopy.length; i++) {
+        if (smallestNumber !== arrayToSortCopy[i]) {
+            tempArray.push(arrayToSortCopy[i])
+        }        
+    }
+    return tempArray
+}
+
+// Process of finding the 'smallestNumber' of 'arrayToSortCopy'.
+const findSmallestNumber = function(theArray) {
+    currentIndex = 0
+    for (let i = 0; i < theArray.length; i++) {
+        if (theArray[currentIndex] >= theArray[nextIndex]) {
+            currentIndex = nextIndex
+        } 
+        nextIndex++        
+    }
+    nextIndex = 0
+    return theArray[currentIndex]
+}
+
+const processSortAscended = function() {
+    let arrayToSortCopy = []
+    arrayToSortCopy = [...arrayToSort,]
+    let counter = arrayToSort.length
+    while (counter !== 0) {
+        // Finding the smallest number of 'arrayToSortCopy'.
+        let smallestNumber = findSmallestNumber(arrayToSortCopy)
+        sortAscendedArray.push(smallestNumber)
+        // Supposed to be a copy of 'arrayToSortCopy' but excluding the 'smallestNumber'.
+        let mutatedArrayToSortCopy = mutateArrayToSortCopy(arrayToSortCopy, smallestNumber)
+        arrayToSortCopy = [...mutatedArrayToSortCopy,]
+        counter--        
+    }    
+}
+
+processSortAscended()
+console.log('sortAscendedArray', sortAscendedArray)
